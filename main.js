b924dae0271a4f55bd60dfe6e6227c0ddab8d117
EVAPorator-sudo <osxar.jeavons@gmail.com>
2026-02-27 20:12:36 +0000

    reformat

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const testButton = document.querySelector("#testButton");
 const dogButton = document.querySelector("#dogButton");
 
-var Dog1Path = "resources/BigDog.jpg";
-var Dog2Path = "resources/LilGuy.jpg";
-var Dog1Name = "Loki:";
-var Dog2Name = "Rocket:";
+const Dog1Path = "resources/BigDog.jpg";
+const Dog2Path = "resources/LilGuy.jpg";
+const Dog1Name = "Loki:";
+const Dog2Name = "Rocket:";
 
 function alertTest(popup) {
     alert(popup);
-};
+}
 
 function dogSwap(dogImage, dogName) {
     const path = dogImage.getAttribute("src");
+
     if (path.includes(Dog1Path)) {
         dogImage.setAttribute("src", Dog2Path);
         dogName.textContent = Dog2Name;
@@ -19,11 +20,12 @@ function dogSwap(dogImage, dogName) {
         dogImage.setAttribute("src", Dog1Path);
         dogName.textContent = Dog1Name;
     }
-};
+}
 
 dogButton.addEventListener("click", () => {
     const dogImage = document.getElementById("DogImage");
     const dogName = document.getElementById("DogName");
+
     dogSwap(dogImage, dogName);
 });
 
